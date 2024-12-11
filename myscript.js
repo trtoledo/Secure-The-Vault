0f30367fbd83eb9ff1bf5e70b91aaa2d3e0469ee
Tomas Toledo <trtoledo1977@gmail.com>
2024-12-11 20:42:50 -0300

    alert

---
+++ myscript.js
@@ -0,0 +1,12 @@
+
+let greeting = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:  "
+let num1 = 100/10;
+let num2 = 8*5;
+let num3 = 50 - 11;
+
+alert(`${greeting + num1 + num2 + num3}`)
+
+
+
+
+//console.log("Tomas")
